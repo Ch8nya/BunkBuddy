c9a0ed7633af718e289e5c1741f464ad531d22be
Ch8nya <90951141+Ch8nya@users.noreply.github.com>
2024-04-16 16:29:01 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PDF to Text Converter</title>
+</head>
+<body>
+    <h1>PDF to Text Converter</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="pdf_file" accept=".pdf">
+        <input type="submit" value="Convert to Text">
+    </form>
+</body>
+</html>
